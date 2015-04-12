74e10a4cf7cdac282909da459c70ce53b6e3fa16
Sharlene <17sharl@gmail.com>
2015-04-12 18:28:24 +0800

    Change snow type between Snowflake/Fairy Dust/Soap Bubble with keyboard command "S"

---
+++ Assets/ChangeSnowType.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeSnowType : MonoBehaviour {
+
+	public Material[] myMaterials = new Material[3];
+	int maxMaterials;
+	int arrayPos = 0;
+
+	// Use this for initialization
+	void Start ()
+	{
+		maxMaterials = myMaterials.Length-1;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Input.GetKeyDown(KeyCode.S))
+		{
+			renderer.material = myMaterials[arrayPos];
+			
+			if(arrayPos == maxMaterials)
+			{
+				arrayPos = 0;
+			}
+			else
+			{
+				arrayPos++;
+			}
+			
+		}  
+		
+	}
+}
+
+
