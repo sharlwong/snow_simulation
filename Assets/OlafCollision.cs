37c4c06c1abe452c74fc1c5fe2c0701fac60c22e
Sharlene <17sharl@gmail.com>
2015-04-17 23:39:56 +0800

    Allow collision between Elsa and Olaf

---
+++ Assets/OlafCollision.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OlafCollision : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter (Collider other) {
+		Destroy(this.gameObject);
+		Renderer[] renderers = other.gameObject.GetComponentsInChildren<Renderer>();
+		foreach (Renderer r in renderers)
+		{
+			r.enabled = true;
+		}
+	}
+}
