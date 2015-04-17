21a156b42c0cff991a219117a8e3e3b4c498f5e2
Wang LiJuan <wanglijuan0531@gmail.com>
2015-04-17 17:03:45 +0800

    add footprint trait rendering and footstep sound

---
+++ Assets/footsound.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class footsound : MonoBehaviour {
+	public AudioClip[] footsteps;
+	public float nextFoot;
+
+	// Use this for initialization
+	IEnumerator Start ()
+	{
+		CharacterController controller = GetComponent<CharacterController>();
+		
+		while(true)
+		{
+			if(controller.isGrounded && controller.velocity.magnitude > 0.3F)
+			{
+				audio.PlayOneShot (footsteps[Random.Range(0, footsteps.Length)]);
+				yield return new WaitForSeconds(nextFoot);
+			}
+			else
+			{
+				yield return 0;
+			}
+		}
+	}
+
+}
