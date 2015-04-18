c3cbe16cea0c41262082da78839290d623929610
ilhaoni <Immanuella_lim@mymail.sutd.edu.sg>
2015-04-18 12:01:05 +0800

    reduced curve swirl size, new prefab, toggle swirl script created

---
+++ Assets/ToggleNewSwirl.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ToggleNewSwirl : MonoBehaviour {
+
+	public GameObject snowSwirl;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+//		if (Input.GetKeyDown (KeyCode.C)) {
+//			var pos
+//		}
+	}
+}
